365185dad30a9bf92ff0b742fb7201f18de62b9e
Teemu Koskinen <koskinen6@gmail.com>
2020-12-16 21:06:18 +0200

    Added functionality to display the total amount of all the names

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Buttons from "./components/Buttons";
 function App() {
+  /**Using this proxy to bypass CORS */
   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const fetchUrl = "https://intense-peak-63735.herokuapp.com/";
 
   const [names, setNames] = useState([]);
 
+  const [namesAmount, setNamesAmount] = useState({});
+
   const [loading, setLoading] = useState(true);
+
+  const fetchNames = () => {
+    fetch(proxyUrl + fetchUrl)
+      .then((res) => res.json())
+      .then((data) => setNames(data.names))
+      .then(() => setLoading(false));
+  };
 
   const amountSort = () => {
     names.sort((a, b) => {
       return b.amount - a.amount;
     });
-    let sorted = [...names];
-    setNames(sorted);
+    setNames(names);
+    setNamesAmount({});
   };
 
   const alphabetSort = () => {
@@ -27,22 +37,42 @@ function App() {
       }
       return 0;
     });
-    let sorted = [...names];
-    setNames(sorted);
+    setNames(names);
+    /**Setting this to be an empty object hides namesAmount object. */
+    setNamesAmount({});
   };
 
+  const amountOfAllNames = () => {
+    let sum = 0;
+    for (let i = 0; i < names.length; i++) {
+      sum += names[i].amount;
+    }
+    setNamesAmount({
+      text: "The total amount of all the names is:",
+      amount: sum,
+    });
+  };
+
+  /** Fetch names when component mounts */
   useEffect(() => {
-    fetch(proxyUrl + fetchUrl)
-      .then((res) => res.json())
-      .then((data) => setNames(data.names))
-      .then(() => setLoading(false));
+    fetchNames();
   }, []);
+
+  useEffect(() => {}, [namesAmount, names]);
 
   return (
     <div>
-      <Buttons amountSort={amountSort} alphabetSort={alphabetSort} />
+      <Buttons
+        amountSort={amountSort}
+        alphabetSort={alphabetSort}
+        amountOfAllNames={amountOfAllNames}
+      />
       {loading ? (
         <p>Loading list of names from an API...</p>
+      ) : namesAmount.hasOwnProperty("text") ? (
+        <p>
+          {namesAmount.text} {namesAmount.amount}
+        </p>
       ) : (
         names.map((name, index) => (
           <p key={index}>
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,21 +1,29 @@
 import React from "react";
 
-function Buttons({ amountSort, alphabetSort }) {
+function Buttons({ amountSort, alphabetSort, amountOfAllNames }) {
   return (
     <div>
+      <p>Sort by: </p>
       <button
         onClick={() => {
           amountSort();
         }}
       >
-        amount
+        Popularity
       </button>
       <button
         onClick={() => {
           alphabetSort();
         }}
       >
-        alphabetically
+        Alphabetical order
+      </button>
+      <button
+        onClick={() => {
+          amountOfAllNames();
+        }}
+      >
+        Total amount of all names
       </button>
     </div>
   );
